65eb4024cbd2b74e4943934c76d0d8af852cf190
Jose de Almeida <josedealmeida930@gmail.com>
2025-10-21 11:04:41 +0100

    Add product editing functionality and update templates
    - Implemented 'editar_produto' view to handle product updates.
    - Updated 'loja.html' to include a form for editing product details.
    - Added a button in 'index.html' to list products.
    - Modified URL patterns to include the new edit route.
    - Changed the title in 'produto.html' to reflect product listing.

--- produtos/urls.py
+++ produtos/urls.py
@@ -4,5 +4,7 @@ from produtos import views
 urlpatterns = [
     path('cadastrar/', views.cadastrar_produto, name='cadastrar_produto'), # type: ignore
     path('', views.visualizar_produtos, name='visualizar_produtos'),
-     path('apagar_produto/<int:produto_id>/', views.apagar_produto, name='apagar_produto'),  # nova rota para apagar produto
+    path('apagar_produto/<int:produto_id>/', views.apagar_produto, name='apagar_produto'),     # nova rota para apagar produto
+    path('editar_produto/<int:produto_id>/', views.editar_produto, name='editar_produto'), # nova rota para editar produto # type: ignore
+          
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -2,6 +2,7 @@ from turtle import st
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Produto
+from decimal import Decimal
 
 
 def cadastrar_produto(request):
@@ -43,3 +44,27 @@ def index(request):
 
 def loja(request):
     return render(request, 'loja.html')  # Exemplo
+
+def editar_produto(request, produto_id):
+    try:
+        produto = Produto.objects.get(id=produto_id)
+    except Produto.DoesNotExist:
+        return HttpResponse("Produto não encontrado.", status=404)
+
+    if request.method == 'GET':
+        return render(request, 'cadastrar.html', {'produto': produto})
+
+    elif request.method == 'POST':
+        nome = request.POST.get('nome_do_produto')
+        preco = (request.POST.get('preco_do_produto'))
+    
+        preco = Decimal(preco.replace(',', '.'))
+        if preco <= 0:
+            ## return redirect(f'/produtos/editar_produto/{produto_id}/?status=0')
+            return HttpResponse("Preço inválido.", status=302)
+
+        produto.nome = nome
+        produto.preco = preco
+        produto.save()
+
+        return redirect('/loja/')
